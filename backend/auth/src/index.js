d9834845bf8381907e1a1c44957874770fbd4ad8
etchichury <joaoetchichury@hotmail.com>
2020-04-15 20:28:49 -0300

    Fix keycloack

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var Keycloak = require('keycloak-connect');
 
+const kcConfig = require('./keycloak.json')
 
-
-const keycloak = require('./config/keycloak');
-const routes = require('./routes/routes');
-
-
+const keycloak = new Keycloak({}, kcConfig);
+keycloak.redirectToLogin = () => false;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(keycloak.middleware());
-app.use(routes);
+
+app.get('/ping', function(req, res) {
+  res.send('pong');
+});
+
+app.post('/login', (req, res) => {
+  keycloak.grantManager
+    .obtainDirectly(req.body.login, req.body.password)
+    .then(grant => {
+      res.json(grant).status(200);
+    })
+    .catch(error => {
+      res.send(error).status(401);
+    });
+});
+
+app.use( keycloak.middleware( { logout: '/'} ));
+
 module.exports.start = port =>
   app.listen(port, () => console.log(`Listening on port ${port}`));
